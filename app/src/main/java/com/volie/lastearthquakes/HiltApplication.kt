ea754a7fa09dad09a00ffad363dc591cd289e638
monster <harunuyan6@gmail.com>
2023-03-15 01:32:35 +0300

    implemented hilt in project

---
+++ app/src/main/java/com/volie/lastearthquakes/HiltApplication.kt
@@ -0,0 +1,7 @@
+package com.volie.lastearthquakes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application()
--- app/src/main/java/com/volie/lastearthquakes/ui/MainActivity.kt
+++ app/src/main/java/com/volie/lastearthquakes/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.volie.lastearthquakes
+package com.volie.lastearthquakes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.volie.lastearthquakes.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
