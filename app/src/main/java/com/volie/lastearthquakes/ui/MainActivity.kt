5b1331d85dce4733fe8a26e7ac0bf9dc8ebcc69f
monster <harunuyan6@gmail.com>
2023-03-15 12:03:47 +0300

    implemented bottom navigation view

--- app/src/main/java/com/volie/lastearthquakes/ui/MainActivity.kt
+++ app/src/main/java/com/volie/lastearthquakes/ui/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.volie.lastearthquakes.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.volie.lastearthquakes.R
+import com.volie.lastearthquakes.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _mBinding: ActivityMainBinding? = null
+    private val mBinding get() = _mBinding!!
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfig =
+            AppBarConfiguration(
+                setOf(
+                    R.id.earthquakeFragment,
+                    R.id.earthquakeRiskMapFragment
+                )
+            )
+
+        mBinding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
     }
 }
